36de62e3635ff582df8b69938b95590c328d7171
zhecai <isirin1131@outlook.com>
2024-08-19 01:15:10 +0800

    tested

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-ln -s ./.vimrc ~/.vimrc
-echo "done." 
+ #!/bin/bash
+ files=$(ls -a $1 | grep -E '^\.[^\.]+' |grep -v .git)
+ # 去掉 ls -a 返回结果中的 ". .. .git and xxxx.xxxx"
+ for file in `echo $files`; do
+     ln $1/$file ~/$file # 创建 hard 链接
+ done
