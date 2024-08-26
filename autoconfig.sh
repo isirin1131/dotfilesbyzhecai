d01946a4fb1775f31d8a3ad77167ea9b412ff4ab
tianzhaotao <13793971886@139.com>
2024-08-26 16:23:01 +0800

    fix bug

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,6 +1,6 @@
- #!/bin/bash
- files=$(ls -a $1 | grep -E '^\.[^\.]+' |grep -v .git)
- # 去掉 ls -a 返回结果中的 ". .. .git and xxxx.xxxx"
- for file in `echo $files`; do
-     ln $1/$file ~/$file # 创建 hard 链接
- done
+#!/bin/bash
+files=$(ls -a $1 | grep -E '^.[^.]+')
+# 去掉 ls -a 返回结果中的 ". .. .git and xxxx.xxxx"
+for file in `echo $files`; do
+	ln $1/$file ~/$file
+done
