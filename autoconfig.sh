f03bd9fe346fe371e5a61a60b33b9e506b829431
tianzhaotao <13793971886@139.com>
2026-01-30 22:15:16 +0800

    feat: 支持递归，支持软链接而非复制文件

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
+
+# 处理 dotfiles 目录中的隐藏文件（除了 . 和 ..）
 files=$(ls -a $1 | grep -E '^.[^.]+')
-# 去掉 ls -a 返回结果中的 ". .. .git and xxxx.xxxx"
 for file in `echo $files`; do
-	cat $1/$file > ~/$file
+    if [ -f "$1/$file" ]; then
+        echo "Creating symlink for $file"
+        ln -sf "$(realpath "$1/$file")" ~/"$file"
+    fi
 done
+
+# 处理 .config 目录
+if [ -d "$1/.config" ]; then
+    echo "Processing .config directory..."
+    # 确保 ~/.config 目录存在
+    mkdir -p ~/.config
+    
+    # 遍历 .config 目录中的所有内容
+    find "$1/.config" -type f | while read -r src_file; do
+        # 计算相对路径
+        rel_path="${src_file#$1/.config/}"
+        dest_file="$HOME/.config/$rel_path"
+        
+        # 确保目标目录存在
+        mkdir -p "$(dirname "$dest_file")"
+        
+        echo "Creating symlink for .config/$rel_path"
+        ln -sf "$(realpath "$src_file")" "$dest_file"
+    done
+fi
+
+echo "Configuration complete!"
---
+++ dotfiles/.config/nvim/init.lua
@@ -0,0 +1,60 @@
+-- Neovim configuration
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Basic settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+
+-- Key mappings
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Plugin management with lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- Add your plugins here
+  "folke/tokyonight.nvim",
+  "nvim-tree/nvim-tree.lua",
+  "nvim-tree/nvim-web-devicons",
+  "nvim-lualine/lualine.nvim",
+  "nvim-treesitter/nvim-treesitter",
+})
+
+-- Theme
+vim.cmd.colorscheme("tokyonight")
