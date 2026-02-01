d291858f17ef59eeb8d8e55528acb465656d759a
tianzhaotao <13793971886@139.com>
2026-02-01 18:28:12 +0800

    feat: more configs

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,37 +1,31 @@
--- Neovim configuration
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Basic settings
+-- line number
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Tab
 vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 0
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.opt.wrap = false
+
+-- file
+vim.opt.autoread = true
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
-vim.opt.hlsearch = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- search
 vim.opt.incsearch = true
+
+-- UI
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
+vim.opt.cursorline = true
 vim.opt.colorcolumn = "80"
 
--- Key mappings
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("x", "<leader>p", "\"_dP")
 
 -- Plugin management with lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -49,12 +43,13 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   -- Add your plugins here
-  "folke/tokyonight.nvim",
+  "EdenEast/nightfox.nvim",
   "nvim-tree/nvim-tree.lua",
   "nvim-tree/nvim-web-devicons",
   "nvim-lualine/lualine.nvim",
   "nvim-treesitter/nvim-treesitter",
+  require("plugins.multicursor")
 })
 
 -- Theme
-vim.cmd.colorscheme("tokyonight")
+vim.cmd.colorscheme("terafox")
